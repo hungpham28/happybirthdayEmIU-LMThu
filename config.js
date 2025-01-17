1a25064f31c020e4f2c616dd1a6b4e7d43de02d7
hungpham28 <hungpham28092003@gmail.com>
2025-01-17 18:51:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,27 @@
+const CONFIG = {
+    titleWeb: "Gửi em gái iu quá chừng",
+    introTitle: 'Cô gái yêu dấu ơi!',
+    introDesc: `Sau nhiều ngày cố giữ mình im lặng và hoàn thành món quà này.
+    Anh thực sự đã cố để hoàn thành món quà này.
+    Anh biết mình không nên như vậy nhưng đó là điều cần thiết cho cả anh và em.
+    Anh muốn em nhận ra vấn đề bởi vì lời nói không thể thay đổi ai.
+    Anh cố gắng chậm rãi để thích em chứ không vội vàng...
+    Anh yêu bằng cách hai người sẽ cùng nhau thay đổi.
+    Chúng ta cố gắng cùng nhau cho nhau cảm giác an toàn.
+    Anh thích nhìn người anh thích trưởng thành và tài giỏi hơn.
+    Anh dám đảm bảo không có việc vì tính cách em mà rời xa trong im lặng.
+    Đừng bao giờ hỏi anh rằng anh còn thích em không vì nó rất vô nghĩa nếu em còn thích anh.
+    Thôi thì bây giờ xin hãy cùng anh đi chơi vào T7 tuần này, bởi anh muốn lại gần em thêm chút.`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng cô gái tui đã biết yêu rồi🥰',
+    desc: 'Thoát web là dắt tôi đi ăn nhé!!!',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không muốn đi :3',
+    question: 'Cô gái của tôi có đồng ý đi cùng tôi trên con đường tiếp theo không <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu anh nhiều lắm luônnnnnnnnnnn <33333333',
+    mess: 'Anh biết mà 🥰. Anh cũng yêu nhiều nhiều lắm😘😘',
+    messDesc: 'Mai 18h anh qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://facebook.com/Hung.IT.5' //link mess của các bạn. VD: /dinhkaito
+}
